f62b8e6736391c91d3ceae7749c99cfdfca74d51
Anton Bortnikov <6088046+ntnbrtnkv@users.noreply.github.com>
2019-07-01 21:34:55 +0500

    refactor: split ports

--- mobx/src/server.js
+++ mobx/src/server.js
@@ -92,6 +92,6 @@ process.on('uncaughtException', evt => {
 	console.log('uncaughtException: ', evt);
 });
 
-app.listen(3000, function(){
-	console.log('Listening on port 3000');
+app.listen(3001, function(){
+	console.log('Listening on port 3001');
 });
